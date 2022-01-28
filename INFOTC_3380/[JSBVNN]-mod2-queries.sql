644dd453daf2cbb80cb19885583f3cfebe590eb2
Josh Block <247jblock@gmail.com>
2022-01-27 22:29:03 -0600

    Updated comments and questions

--- INFOTC_3380/[JSBVNN]-mod2-queries.sql
+++ INFOTC_3380/[JSBVNN]-mod2-queries.sql
@@ -1,6 +1,14 @@
---Josh Block
---1/27/22
---v1.0.2
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 1/27/22
+Version: 1.0.3 ***/
+
+--RECALL tables
+SHOW tables;
+--RECALL Tables_in_module2
+DESCRIBE agents;
+DESCRIBE customer;
+DESCRIBE orders;
 
 --Display all of the customer data. (25 rows returned)
 SELECT * FROM customer;
@@ -13,11 +21,14 @@ SELECT agent_code, agent_name, working_area FROM agents;
 --Display order number, order amount, and order date. (34 rows returned)
 SELECT ord_num, ord_amount, ord_date FROM orders;
 --Show all customer code, name, country, and phone number, for all customers from the United States. (4 rows returned)
-SELECT * FROM customers;
+SELECT * FROM customer;
 SELECT cust_code, cust_name, cust_country, phone_no, FROM customer WHERE cust_country='USA';
-
 --Show agent names and phone numbers for agents from Bangalore. (3 rows returned)
+SELECT * FROM agents;
+SELECT agent_name, phone_no FORM agents WHERE working_area='Bangalore';
 --Show orders with advance amounts of $100. (6 rows returned)
+SHOW * FROM orders;
+SELECT * FROM orders WHERE advance_amount='100';
 --Show the names of grade 2 customers. (10 rows returned)
 --Show all customer information for customers from New York. (3 rows returned)
 --Show all agent information for agents who work in London with commission rates greater than 0.12. (2 rows returned)
