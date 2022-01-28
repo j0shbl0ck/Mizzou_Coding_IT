536b8d89ccf5cf5bcc306fb45873cc3f7dd73fc5
Josh Block <247jblock@gmail.com>
2022-01-27 21:40:40 -0600

    Added queries in question

--- INFOTC_3380/[JSBVNN]-mod2-queries.sql
+++ INFOTC_3380/[JSBVNN]-mod2-queries.sql
@@ -1,2 +1,19 @@
 --Josh Block
 --1/27/22
+--v1.0.0
+
+--Display all of the customer data. (25 rows returned)
+--Display all of the agent data. (12 rows returned)
+--Display customer names and grades. (25 rows returned)
+--Display agent code, name, and working area. (12 rows returned)
+--Display order number, order amount, and order date. (34 rows returned)
+--Show all customer code, name, country, and phone number, for all customers from the United States. (4 rows returned)
+--Show agent names and phone numbers for agents from Bangalore. (3 rows returned)
+--Show orders with advance amounts of $100. (6 rows returned)
+--Show the names of grade 2 customers. (10 rows returned)
+--Show all customer information for customers from New York. (3 rows returned)
+--Show all agent information for agents who work in London with commission rates greater than 0.12. (2 rows returned)
+--Show order number, agent code, and customer code for orders with advance amount less than or equal to $500, and order amount greater than or equal to $1500. (9 rows returned)
+--Show customer code, customer name, agent code, and outstanding amount for customers with outstanding amounts greater than $6000 and pay amount less tha or equal to $3000. (3 rows returned)
+--Show order numbers for order amounts less than $1000. (8 rows returned)
+--Show customer names from India with outstanding amounts less than $10,000. (5 rows returned)
