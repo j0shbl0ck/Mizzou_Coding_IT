d00f6bbc2aabb077a2b8b13265af19bf1bd3a64a
Josh Block <247jblock@gmail.com>
2022-02-03 20:20:18 -0600

    Uploaded file

---
+++ INFOTC_3380/[JSBVNN]-mod3-queries.sql
@@ -0,0 +1,11 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 2/03/22
+Version: 1.0.0 ***/
+
+--RECALL tables
+SHOW tables;
+--RECALL Tables_in_module2
+DESCRIBE agents;
+DESCRIBE customer;
+DESCRIBE orders;
