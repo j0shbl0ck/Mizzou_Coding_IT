f4a3882322d8d248211ba1d42fbba4a1e7d352f5
Josh Block <247jblock@gmail.com>
2022-02-10 19:02:37 -0600

    Added queries

--- INFOTC_3380/stocks_zips_[JSBVNN].js
+++ INFOTC_3380/stocks_zips_[JSBVNN].js
@@ -1,10 +1,29 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/10/22
-Version: 1.0.0 ***/
+Version: 1.0.1 ***/
 
 // RECALL the collections
 show collections
 // RECALL structure of the data in your collections
 db.zips.findOne();
 db.stocks.findOne();
+
+// List all zip codes and city from your home state.
+// List all of the zip codes and cities from the states of Missouri, Florida, Iowa, and Kansas.
+// List the zip codes, city, and population in Ohio for all the zip codes with populations of less than 2000 people?
+// List the zip codes, city, and population for zip codes in the United States that have populations greater than 50,000 people?
+// Which zip codes are located in the city of Columbia, Missouri?
+// List the population by zip code for all zip codes in the state of Missouri.
+// Which states have cities named Springfield?
+// How many cities named Springfield are there in the United States?
+// List the zip code, population, and state for cities named Emerald.
+// List the zip code, city, and state for cities with a population of 10 people.
+// List all of the documents in the stocks collection.
+// List the company name and symbol for all companies in the “Information Technology” sector.
+// List the symbol, company name, and stock price for all companies in the “Financials” sector whose stock price is greater than $75.
+// List the Company name, sector, and earnings (EBITDA) for all companies with earnings (EBITDA) of at least $2.5 Billion
+// List the company name, symbol and sector for all companies with a 52 Week High greater than or equal to 150.
+// List the symbol, company name, and stock price for all companies in the “Real Estate” sector whose stock price is less than $50.
+// List the company name, symbol and dividend yield for companies in the Utilities or Industrials sectors.
+// List the company name, symbol and market cap for companies whose dividend yield is greater than 3 or whose earnings/share is less than 1.
