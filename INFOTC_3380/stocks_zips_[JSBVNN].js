99e780933ddeedbc08ccff3f92057233b9c5499a
Josh Block <247jblock@gmail.com>
2022-02-10 18:58:42 -0600

    Uploaded file

---
+++ INFOTC_3380/stocks_zips_[JSBVNN].js
@@ -0,0 +1,11 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 1/27/22
+Version: 1.0.5 ***/
+
+--RECALL tables
+SHOW tables;
+--RECALL Tables_in_module2
+DESCRIBE agents;
+DESCRIBE customer;
+DESCRIBE orders;
