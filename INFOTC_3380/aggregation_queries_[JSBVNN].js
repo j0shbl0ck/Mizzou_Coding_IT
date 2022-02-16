1e0a44a02550275df87586637521d2b34e2be020
Josh Block <247jblock@gmail.com>
2022-02-15 20:33:17 -0600

    Uploaded new file 🆙

---
+++ INFOTC_3380/aggregation_queries_[JSBVNN].js
@@ -0,0 +1,13 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 2/15/22
+Version: 1.0.0 ***/
+
+/* 
+RECALL the collections:
+show collections
+-------------------
+RECALL structure of the data in your collections:
+db.zips.findOne();
+db.stocks.findOne();
+*/
