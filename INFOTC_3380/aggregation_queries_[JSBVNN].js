06da1de1a9117d4754d85201d1fa9290a3ad019b
Josh Block <247jblock@gmail.com>
2022-02-17 17:04:38 -0600

    🛠️ updated answer

--- INFOTC_3380/aggregation_queries_[JSBVNN].js
+++ INFOTC_3380/aggregation_queries_[JSBVNN].js
@@ -22,7 +22,7 @@ db.zips.aggregate([{$match:{state:"FL"}},{$group:{"_id":"$city",TotalPopulation:
 // What are the 10 most populous cities in MO?
 db.zips.aggregate([{$match:{state:"MO"}},{$group: {"_id":"$city",TotalPopulation:{$sum:"$pop"}}},{$project: {city:1,TotalPopulation: 1}},{$sort:{TotalPopulation: -1}},{$limit: 10}])
 // What is the population of New York City, NY?
-db.zips.aggregate([{$match:{state:"NY",city:"NEW YORK CITY"}},{$group:{TotalPopulation:{$sum:"$pop"}}},{$project:{TotalPopulation:1}}])
+db.zips.aggregate([{$match:{state:"NY",city:"NEW YORK CITY"}},{$group:{"_id":null,TotalPopulation:{$sum:"$pop"}}},{$project:{TotalPopulation:1}}])
 // List the cities in Illinois that have 3 or more zip codes? Sort in descending order by total number of zip codes. Hint: count multiple occurrences of a city’s name.
 // Which city has the fewest number of zip codes?
 // What is the name and total population of the most populous city in the zips database?
