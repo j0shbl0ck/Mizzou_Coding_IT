aad882cc25b74517f51237f49661ef018d13833e
Josh Block <247jblock@gmail.com>
2022-02-17 14:00:54 -0600

    ⬆️ uploaded answer

--- INFOTC_3380/aggregation_queries_[JSBVNN].js
+++ INFOTC_3380/aggregation_queries_[JSBVNN].js
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/15/22
-Version: 1.0.3 ***/
+Version: 1.0.4 ***/
 
 /* 
 RECALL the collections:
@@ -13,10 +13,11 @@ db.stocks.findOne();
 */
 
 // Which states have zip codes with populations greater than (75,000) people?
-db.zips.aggreate([{match:{pop:{$gt:75000}}},{$project: {state:1}}]);
+db.zips.aggreate([{$match:{pop:{$gt:75000}}},{$project: {state:1}}]);
 // Which cities have populations greater than 200,000 people?
 db.zips.aggregate([ {$group: {city: "$city"}}, {$math : {pop:{$gt:200000}}},{$project:{_id:0,city:1}}]);
 // What is the total population of each city in FL. Sort in ascending order based on total population?
+db.zips.aggreate([{$match:{state:"FL"}},{$group:{_id:"city",TotalPopulation:{$sum: "$pop"}}},{$sort: {TotalPopulation: {$sum: "$pop"}}}])
 // What are the 10 most populous cities in MO?
 // What is the population of New York City, NY?
 // List the cities in Illinois that have 3 or more zip codes? Sort in descending order by total number of zip codes. Hint: count multiple occurrences of a city’s name.
