cec00270cb45fd250afdb80f604e7b3b76a6e0f9
Josh Block <247jblock@gmail.com>
2022-02-25 21:54:52 -0600

    ⬆️ uploaded answers

--- INFOTC_3380/mod6_queries_[JSBVNN].sql
+++ INFOTC_3380/mod6_queries_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/22/22
-Version: 1.0.1 ***/
+Version: 1.0.2 ***/
 
 --RECALL tables
 SHOW tables;
@@ -38,20 +38,43 @@ LIMIT 25;
 SELECT YEAR(paymentDate), SUM(amount) AS "Total Payments" --could also refer to how many payments people made
 FROM payments
 GROUP BY YEAR(paymentDate)
-ORDER BY SUM(amount);
+ORDER BY SUM(amount); 
 -- Calculate the total payments made each month in 2004. Display the month and total payments in a column called “Total Payments”. Order the results by month in ascending order. Note: you will have to use the MONTH() and YEAR() functions.
-
+SELECT MONTH(paymentDate),SUM(amount) AS "Total Payments"
+FROM payments
+WHERE YEAR(paymentDate) = 2004
+GROUP BY MONTH(paymentDate)
+ORDER BY MONTH(paymentDate) ASC;
 -- Calculate the total payments made each day in December of 2004. Display the day in a column named “Day” and total payments in a column called “Total Payments”. Order the results by day in ascending order. Note: you will have to use the MONTH(), DAY(), and YEAR() functions.
-
+SELECT DAY(paymentDate),SUM(amount) AS "Total Payments"
+FROM payments
+WHERE YEAR(paymentDate) = 2004 && MONTH(paymentDate) = 12
+GROUP BY DAY(paymentDate)
+ORDER BY DAY(paymentDate) ASC;
 -- Calculate the total payments made by each customer in the database who has made a payment. Display the customer name and the total payments in a column named “Total Payments”. Order the results by total payments.
-
+SELECT c.customerName, COUNT(p.amount) AS "Total Payments"
+FROM payments p, customers c
+WHERE c.customerNumber = p.customerNumber
+GROUP BY c.customerName
+ORDER BY COUNT(p.amount);
 -- Calculate and display the number of customers in each state. Display the state name and and number of customers in each state in a column called “Number of Customers in State”. Sort the results by the Number of Customers in State.
-
+SELECT c.state, COUNT(c.customerNumber) AS "Number of Customers in State"
+FROM customers c
+GROUP BY c.state
+ORDER BY COUNT(c.customerNumber);
 -- Which employees manage the most people? Develop a query to calculate the number of people each employees manages. Display the employee number and number of employees employees they manage in a column called “Number of Reports”. Hint: we know an employee manages another employee if their employee number appears in the reportsTo column in the employee table.
-
+SELECT e.reportsTo, COUNT(e.employeeNumber) AS "Number of Reports"
+FROM employees e
+GROUP BY e.reportsTo
+ORDER BY COUNT(e.employeeNumber) DESC;
 -- Write a query to calculate the number of product lines in the database. Display the result in a column called “Number of Lines”.
-
+SELECT COUNT(p.productLine) AS "Number of Lines"
+FROM productlines p;
 -- Calculate the dollar value of each product in inventory. You can calculate this by multiplying the quantity in stock by the buy price. Display the product name, quantity in stock, buy price, and in its dollar value in a column called “Dollar Value”. Sort the results in descending order based on dollar value.
+SELECT e.reportsTo, COUNT(e.employeeNumber) AS "Number of Reports"
+FROM employees e
+GROUP BY e.reportsTo
+ORDER BY COUNT(e.employeeNumber) DESC;
 
 
 https://stackoverflow.com/questions/9246410/mysql-query-find-customer-w-orders-but-without-payment
