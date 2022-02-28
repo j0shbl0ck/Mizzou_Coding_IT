83f2d05383047bae21658c641303a0d9eb109598
Josh Block <247jblock@gmail.com>
2022-02-27 22:39:10 -0600

    ➕ added new folder and files

---
+++ INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
@@ -0,0 +1,18 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 2/22/22
+Version: 1.0.4 ***/
+
+--RECALL tables
+SHOW tables;
+--RECALL Tables_in_classicmodels
+DESCRIBE <table in classicmodels>;
+
+-- List all of the current employees first name, last name, salary and department name.
+-- List the first name, last name, and salary of the current employees with the ten (10) highest salaries.
+-- What is the total payroll (sum of all current salaries) for the company.
+-- Display a list of the unique titles for this company.
+-- List the first name, last name, and department name for all current department managers. Remember that a current manager has a to_date value of 9999-01-01.
+-- How many employees does each department currently have? List the department name and number of employees in a column named “Number of Employees”.
+-- How many males and how many females have been hired by this company in its history?
+-- List the titles and the number of current employees that hold that title.
---
+++ INFOTC_3380/project1/project1_mongo_queries_[JSBVNN].js
@@ -0,0 +1,24 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 2/27/22
+Version: 1.0.0 ***/
+
+/* 
+RECALL the collections:
+show collections
+-------------------
+RECALL structure of the data in your collections:
+db.orders.findOne();
+db.customers.findOne();
+db.products.findOne();
+db.employees.findOne();
+*/
+
+// How many products have an MSRP of $100 of greater?
+// Calculate the number of orders each customer has placed and display the top 25 in descending order based on orders placed. Display the customer name and the orders placed in a columns called “Orders Placed”.
+// List the product names where the quantity in stock is greater than 5000
+// Calculate and display the number of customers in each state. Display the customer name and and number of customers in each state in a column called “Number of Customers in State”. Sort the results by the Number of Customers in State.
+// Which employees manage the most people? Develop a query to calculate the number of people each employees manages. Display the employee number and number of employees employees they manage in a column called “Number of Reports”.
+// List the names and credit limit of the customers with the 10 highest credit limits
+// Write a query to calculate the number of product lines in the database. Display the result in a column called “Number of Lines”. 
+// Calculate the dollar value of each product in inventory. You can calculate this by multiplying the quantity in stock by the buy price. Display the product name, quantity in stock, buy price, and in its dollar value in a column called “Dollar Value”. Sort the results in descending order based on dollar value.
