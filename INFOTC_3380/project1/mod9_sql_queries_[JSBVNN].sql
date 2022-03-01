33f4b3cbbef0994d97fbc176cb9656550ed8b9af
Josh Block <247jblock@gmail.com>
2022-02-28 22:26:59 -0600

    ⬆️  uploaded answer

--- INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
+++ INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.0.3 ***/
+Version: 1.0.5 ***/
 
 --RECALL tables
 SHOW tables;
@@ -9,19 +9,27 @@ SHOW tables;
 DESCRIBE <table in employees>;
 
 -- List all of the current employees first name, last name, salary and department name.
-SELECT e.first_name, e.last_name, SUM(s.salary), d.dept_name
+SELECT e.first_name, e.last_name, SUM(s.salary) AS "salary", d.dept_name
 FROM employees e, salaries s, departments d
 WHERE e.emp_no = s.emp_no
-GROUP BY e.first_name && e.last_name;
+GROUP BY e.first_name, e.last_name, d.dept_name
+ORDER BY SUM(s.salary) DESC
+LIMIT 10;
 -- List the first name, last name, and salary of the current employees with the ten (10) highest salaries.
-SELECT e.first_name, e.last_name, s.salary
+SELECT e.first_name, e.last_name, SUM(s.salary)
 FROM employees e, salaries s
 WHERE e.emp_no = s.emp_no
-GROUP BY e.first_name && e.last_name
-ORDER BY s.salary DESC
+GROUP BY e.first_name, e.last_name
+ORDER BY SUM(s.salary) DESC
 LIMIT 10;
 -- What is the total payroll (sum of all current salaries) for the company.
+SELECT SUM(s.salary) AS "salary"
+FROM salaries s;
 -- Display a list of the unique titles for this company.
+SELECT DISTINCT(t.title)
+FROM titles
+ORDER BY t.title DESC;
+
 -- List the first name, last name, and department name for all current department managers. Remember that a current manager has a to_date value of 9999-01-01.
 -- How many employees does each department currently have? List the department name and number of employees in a column named “Number of Employees”.
 -- How many males and how many females have been hired by this company in its history?
