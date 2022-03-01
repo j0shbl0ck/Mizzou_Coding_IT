e6fcc5770baa4e08a24bf39721d9bcd1516491ef
Josh Block <247jblock@gmail.com>
2022-02-28 23:06:39 -0600

    ⚒️  fixing answer

--- INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
+++ INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.0.8 ***/
+Version: 1.0.9 ***/
 
 --RECALL tables
 SHOW tables;
@@ -33,7 +33,7 @@ ORDER BY t.title DESC;
 -- List the first name, last name, and department name for all current department managers. Remember that a current manager has a to_date value of 9999-01-01.
 SELECT e.first_name, e.last_name, d.dept_name
 FROM employees e, 
-INNER JOIN departments d
+JOIN departments d
 ON (d.dept_no = e.emp_no);
 -- How many employees does each department currently have? List the department name and number of employees in a column named “Number of Employees”.
 -- How many males and how many females have been hired by this company in its history?
