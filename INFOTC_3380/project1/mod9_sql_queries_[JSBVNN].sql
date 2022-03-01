3bf3d38795b61b246079420dc6bf03d0d7936385
Josh Block <247jblock@gmail.com>
2022-02-28 20:28:01 -0600

    ⚒️  minor fix

--- INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
+++ INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.0.1 ***/
+Version: 1.0.2 ***/
 
 --RECALL tables
 SHOW tables;
@@ -11,10 +11,10 @@ DESCRIBE <table in employees>;
 -- List all of the current employees first name, last name, salary and department name.
 SELECT e.first_name, e.last_name, s.salary, d.dept_name
 FROM employees e, salaries s, departments d
-WHERE e.emp_no = s.emp_no
+WHERE e.emp_no = s.emp_no;
 -- List the first name, last name, and salary of the current employees with the ten (10) highest salaries.
 SELECT e.first_name, e.last_name, s.salary
-FROM employees e, salaries s,
+FROM employees e, salaries s
 WHERE e.emp_no = s.emp_no
 ORDER BY COUNT(s.salary) ASC
 LIMIT 10;
