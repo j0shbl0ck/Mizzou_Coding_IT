5ca100c75a21e6800fc7a4a9ee87ee48247b4072
Josh Block <247jblock@gmail.com>
2022-02-28 23:20:38 -0600

    ⬆️  uploaded answer

--- INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
+++ INFOTC_3380/project1/mod9_sql_queries_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.1.1 ***/
+Version: 1.1.2 ***/
 
 --RECALL tables
 SHOW tables;
@@ -37,6 +37,10 @@ WHERE e.emp_no = f.emp_no, f.to_date > (SELECT 9999-01-01 FROM deparments f)
 GROUP BY e.first_name, e.last_name
 
 -- How many employees does each department currently have? List the department name and number of employees in a column named “Number of Employees”.
+SELECT d.dept_name, COUNT(d.dept_no) AS "Number of Employees"
+FROM departments
+GROUP BY d.dept_name
+ORDER BY COUNT(d.dept_no);
 -- How many males and how many females have been hired by this company in its history?
 -- List the titles and the number of current employees that hold that title.
 
