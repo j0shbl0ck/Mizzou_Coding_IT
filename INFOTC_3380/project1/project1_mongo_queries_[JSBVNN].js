e6a16f2e87149817aaf33b3cd4cbf63858160095
Josh Block <247jblock@gmail.com>
2022-03-04 23:02:09 -0600

    ⚒️  fixed answer

--- INFOTC_3380/project1/project1_mongo_queries_[JSBVNN].js
+++ INFOTC_3380/project1/project1_mongo_queries_[JSBVNN].js
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.4.1 ***/
+Version: 1.4.2 ***/
 
 /* 
 RECALL the collections:
@@ -53,7 +53,10 @@ db.products.aggregate([
     {$project:{_id:0,NumberofProductLines:1}}
 ])
 
-db.products.count({"productLine":1,"_id":0})
+db.products.aggregate([
+    {$count:{_id:"$productLine",NumberofProductLines:{$sum:1}}},
+    {$project:{_id:0,NumberofProductLines:1}}
+])
 // Calculate the dollar value of each product in inventory. You can calculate this by multiplying the quantity in stock by the buy price. Display the product name, quantity in stock, buy price, and in its dollar value in a column called “Dollar Value”. Sort the results in descending order based on dollar value.
 db.products.aggregate([
     {$project:{_id:0,productName:1,quantityInStock:1,buyPrice:1,DollarValue:{$multiply:["$quantityInStock","$buyPrice"]}}},
