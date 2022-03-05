ecdb42f1ebdab79cdf8a58845b329c0a34997b9b
Josh Block <247jblock@gmail.com>
2022-03-04 20:28:48 -0600

    ⚒️  fixed answer

--- INFOTC_3380/project1/project1_sql_queries_[JSBVNN].sql
+++ INFOTC_3380/project1/project1_sql_queries_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.3.1 ***/
+Version: 1.3.2 ***/
 
 --RECALL tables
 SHOW tables;
@@ -15,10 +15,10 @@ WHERE e.emp_no = s.emp_no
 GROUP BY e.first_name, e.last_name, d.dept_name
 
 -- List the first name, last name, and salary of the current employees with the ten (10) highest salaries.
-SELECT DISTINCT(e.first_name, e.last_name), s.salary
+SELECT DISTINCT e.first_name, e.last_name, s.salary
 FROM employees e, salaries s
 WHERE e.emp_no = s.emp_no
-ORDER BY s.salary DESC
+ORDER BY e.first_name, e.last_name, s.salary DESC
 LIMIT 10;
 
 -- What is the total payroll (sum of all current salaries) for the company.
