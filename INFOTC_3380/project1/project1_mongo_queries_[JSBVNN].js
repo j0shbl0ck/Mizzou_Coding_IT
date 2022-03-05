68ae5f3e8ca9ad8f5a19a32852a5a3a58dfde264
Josh Block <247jblock@gmail.com>
2022-03-04 22:36:05 -0600

    ⚒️  fixed answer

--- INFOTC_3380/project1/project1_mongo_queries_[JSBVNN].js
+++ INFOTC_3380/project1/project1_mongo_queries_[JSBVNN].js
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 2/27/22
-Version: 1.3.4 ***/
+Version: 1.3.5 ***/
 
 /* 
 RECALL the collections:
@@ -43,7 +43,8 @@ db.employees.aggregate([
 // List the names and credit limit of the customers with the 10 highest credit limits
 db.customers.aggregate([
     {$project:{_id:0,customerName:1,creditLimit:1}},
-    {$sort:{creditLimit:-1}}
+    {$sort:{creditLimit:-1}},
+    {$limit: 10}
 ])
 // Write a query to calculate the number of product lines in the database. Display the result in a column called “Number of Lines”. 
 db.products.aggregate([
