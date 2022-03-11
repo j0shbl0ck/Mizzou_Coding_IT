fb5140d94c17d1be3a65592d22c1e48a48643675
Josh Block <247jblock@gmail.com>
2022-03-11 02:18:47 -0600

    ⬆️   uploaded answers

--- INFOTC_3380/mod8_[JSBVNN].js
+++ INFOTC_3380/mod8_[JSBVNN].js
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 3/09/22
-Version: 1.0.2 ***/
+Version: 1.0.3 ***/
 
 /* 
 RECALL the collections:
@@ -53,17 +53,36 @@ db.customers.aggregate([
 // Calculate the total payments made each month in 2004. Display the month and total payments in a column called “Total Payments”. Order the results by month in ascending order. **Hint: You will need to use the $addFields to add a field for year using the $year function, then match for 2004
 db.customers.aggregate([
     {$unwind:"$payments"},
-    {$project:{_id:{$year:"$payments.paymentDate"},month:{$month:"$payments.paymentDate"},TotalPayments:{$sum:1}}},
-    {$match:{_id:2004}},
-    {$sort:{month: -1}}
+    {$addFields:{"year":{$year: "$payments.paymentDate"}}},
+    {$match:{"year": 2004}},
+    {$group: {_id:{$month: "$payments.paymentDate"}, TotalPayments: {$sum: 1}}},
+    {$sort: {_id: 1}}
+])
 
 // Calculate the total payments made each day in December of 2004. Display the day in a column named “Day” and total payments in a column called “Total Payments”. Order the results by day in ascending order. **Hint: You will need to use the $addFields to add a field for year and month, using the $year and $month functions, then match for 2004 and 12
+db.customers.aggregate([
+    {$unwind:"$payments"},
+    {$addFields:{"year":{$year: "$payments.paymentDate"},"month":{$month: "$payments.paymentDate"}}},
+    {$match:{"year": 2004,"month":12}},
+    {$group: {_id:{$dayOfMonth: "$payments.paymentDate"}, TotalPayments: {$sum: 1}}},
+    {$sort: {_id: 1}}
+])
 
 // Calculate the total payments made by each customer in the database who has made a payment. Display the customer name and the total payments in a column named “Total Payments”. Order the results by total payments.
+db.customers.aggregate([
+    {$group:{_id:"$customerName",TotalPayments:{$sum:1}}}
+])
 
 // Calculate and display the number of customers in each state. Display the customer name and and number of customers in each state in a column called “Number of Customers in State”. Sort the results by the Number of Customers in State.
+db.customers.aggregate([
+    {$group:{_id:"$state",NumberofCustomersinState:{$sum:1}}}
+])
 
 // Which employees manage the most people? Develop a query to calculate the number of people each employees manages. Display the employee number and number of employees employees they manage in a column called “Number of Reports”.
+db.employees.aggregate([
+    {$group:{_id:"$reportsTo",NumberofReports:{$sum:1}}},
+    {$sort: {_id:1}}
+])
 
 // Write a query to calculate the number of product lines in the database. Display the result in a column called “Number of Lines”.
 
