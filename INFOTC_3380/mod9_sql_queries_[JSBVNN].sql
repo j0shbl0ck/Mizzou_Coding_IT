d288ecf3176c8fc261c8fe569833856bfb501c59
Josh Block <247jblock@gmail.com>
2022-03-15 21:55:01 -0500

    ⬆️  uploaded file

---
+++ INFOTC_3380/mod9_sql_queries_[JSBVNN].sql
@@ -0,0 +1,25 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 3/15/22
+Version: 1.0.0 ***/
+
+--RECALL tables
+SHOW tables;
+--RECALL Tables_in_classicmodels
+DESCRIBE <table in sakila>;
+
+-- Display the customer first name, last name, and movie titles the rented in 2006. Sort result by customer last name.
+
+-- How many films are in each category? List the category name in a column called "Category Name" and number of films in the category in a column called "Number of Films". Sort by number of films in descending order
+
+-- Calculate each customer's total payments and display the top 15 customers in terms of total payments in descending order. Display the customer first and last name and total payment amount in a column called "Total Payments".
+
+-- Calculate the total payments for each store. Display store id and total payments in a Column called Total Payments. Display results in descending order by total payments
+
+-- Which films are rented the most? Display the Film name and the total rentals in a column called "Total Rentals". Display results in descending order by total rentals.
+
+-- Which Film categories are rented the most? Display the category name and the total rentals in a column called "Total Rentals". Display results in descending order by total rentals.
+
+-- Display the inventory list of movie titles for store number 1. Display the film title and quantity in stock in a column named "In Stock". Display results in alphabetical order of the titles
+ 
+-- Which actors have acted in the most movies? List the actor first and last name and the number of movies in a column named "Number of Movies". Display results in descending order by number of movies.
