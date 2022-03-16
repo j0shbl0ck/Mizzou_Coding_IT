b35f5bd96d424f7d991d48d93037cfa62d7a4df2
Josh Block <247jblock@gmail.com>
2022-03-15 21:50:02 -0500

    ⬆️  uploaded file

---
+++ INFOTC_3380/mod9_mongo_queries_[JSBVNN].js
@@ -0,0 +1,33 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 3/15/22
+Version: 1.0.0 ***/
+
+/* 
+RECALL the collections:
+USE classicmodels;
+USE sakila;
+SHOW tables;
+-------------------
+RECALL structure of the data in your collections:
+db.orders.findOne();
+db.customers.findOne();
+db.products.findOne();
+db.employees.findOne();
+*/
+
+// How many orders has each order line appeared in? Display order line number and number of orders.
+
+// Calculate the total payments each customer has made. Display customer name and the total payment.
+
+// Calculate the number of payments each customer has made. Display customer name and the number of payments.
+
+// How many employees work in each office. List the office code and number of employees.
+
+// What are the 10 most expensive orders? Display the Customer name and order total and display results in descending order by order total.
+
+// What is the sales value of each product in stock? Sale value is calculated by multiplying quantity in stock by MSRP. Display the product name and the sales value in descending order based on sales value.
+
+// Which Customer has ordered the most "1913 Ford Model T Speedster". Display the customer name and total ordered.
+
+// How many customers does each sales rep have? Display the Sales rep name and the number of customers.
