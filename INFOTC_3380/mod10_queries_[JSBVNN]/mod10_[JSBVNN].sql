3364330f6ac2a6b02db5270e94ffb8ef2ca7c86f
Josh Block <247jblock@gmail.com>
2022-03-24 12:20:15 -0500

    ⬆️  uploaded file

---
+++ INFOTC_3380/mod10_queries_[JSBVNN]/mod10_[JSBVNN].sql
@@ -0,0 +1,29 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 3/24/22
+Version: 1.0.0 ***/
+
+--RECALL tables
+SHOW tables;
+--RECALL Tables_in_classicmodels
+DESCRIBE <table in classicmodels>;
+
+--Write queries using subqueries for the following questions.
+
+-- Write a query to show the customer number, name, payment date, and payment amount for payments greater than the average payment.
+-- Create a query to generate a list of managers. Show their employee number, first name, and last name.
+-- Which offices have the same number of employees as the London office? Show the city, office code and number of employees.
+-- Which product(s) have a higher quantity ordered than “1940s Ford truck”? Show the product names and quantity ordered.
+-- Show the products that have a lower dollar value in orders than the “1957 Corvette Convertible”. Show the product name and total value.
+-- Show the order number, customer number, and order total for orders with a larger order total than order number 10222.
+
+
+
+--Write queries using wildcards for the following questions.
+
+-- Show the company name and total payments for companies whose name ends in “Ltd”.
+-- How many employees have a three digit extension? A three digit extension looks like xXXX.
+-- Show the product code, name, vendor, and buy price for products from the 1950s (1950 - 1959). The year appears is in the name.
+-- show all office information for offices in the 212 area code. Hint. Look at the phone number.
+-- Show first name, last name, employee number and email for the sales managers.
+-- Show the name, product code, quantity in stock, and buy price for products with “Chevy” in the name.
