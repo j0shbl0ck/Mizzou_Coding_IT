220957db061c815bf28c042383d59fdb804b9e20
Josh Block <247jblock@gmail.com>
2022-04-03 21:59:14 -0500

    ⬆️   uploaded folder and code

---
+++ INFOTC_3380/Views_[JSBVNN]/views_[JSBVNN].js
@@ -0,0 +1,17 @@
+/*** USER INFORMATION 
+Student: Josh Block
+Date: 4/03/22
+Version: 1.0.0 ***/
+
+/* 
+RECALL the collections:
+USE classicmodels
+show collections
+-------------------
+RECALL structure of the data in your collections:
+db.orders.findOne();
+db.customers.findOne();
+db.products.findOne();
+db.employees.findOne();
+*/
+
