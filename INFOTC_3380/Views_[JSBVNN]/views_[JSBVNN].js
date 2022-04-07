d7891415322e655b89fa6e0294a8116b2c073c01
Josh Block <247jblock@gmail.com>
2022-04-06 22:35:43 -0500

    updated answers

--- INFOTC_3380/Views_[JSBVNN]/views_[JSBVNN].js
+++ INFOTC_3380/Views_[JSBVNN]/views_[JSBVNN].js
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 4/03/22
-Version: 1.0.2 ***/
+Version: 1.0.3 ***/
 
 /* 
 RECALL the collections:
@@ -18,65 +18,6 @@ To drop a colllection/view
 db.<collection/view>.drop();
 
 */
----------------
-// Create a view named MissouriCities to show the population of all the cities in Missouri based on the data in the zips collection
-db.createView(
-    "MissouriCities",
-    "zips",
-    [
-        {$match: {state: "MO"}},
-        {$group: {_id: "$city", TotalPopulation: {$sum: "$pop"}}},
-        {$sort: {TotalPopulation: -1}}
-    ]
-)
-    // query the MissouriCities view to find cities with populations of 100,000 or greater
-    db.MissouriCities.aggregate([
-        {$match: {TotalPopulation:{$gte:100000}}}
-    ])
-
-// create a view using the stocks collection to show the total EBITDA for each sector.
-db.createView(
-    "EBITDAbySector",
-    "stocks",
-    [ 
-        {$group: {_id: "$Sector", TotalEarnings: {$sum: "$EBITDA"}}}  
-    ]
-)
-    // query the EBTDAbySector view to find the total EBITDA for the "Health Care" Sector.
-    db.EBITDAbySector.find({"_id":"Health Care"})
-
-// Calculate the total payments made each year. Display the year and total payments in a column called “Total Payments”.
-db.customers.aggregate([
-    {$unwind:"$payments"},
-    {$group: {_id:{$year:"$payments.paymentDate"},TotalPayments:{$sum:1}}}
-])
-
-// Calculate the total payments made each month in 2004. Display the month and total payments in a column called “Total Payments”. Order the results by month in ascending order. **Hint: You will need to use the $addFields to add a field for year using the $year function, then match for 2004
-db.customers.aggregate([
-    {$unwind:"$payments"},
-    {$addFields:{"year":{$year: "$payments.paymentDate"}}},
-    {$match:{"year": 2004}},
-    {$group: {_id:{$month: "$payments.paymentDate"}, TotalPayments: {$sum: 1}}},
-    {$sort: {_id: 1}}
-])
-
-// Calculate the total payments made each day in December of 2004. Display the day in a column named “Day” and total payments in a column called “Total Payments”. Order the results by day in ascending order. **Hint: You will need to use the $addFields to add a field for year and month, using the $year and $month functions, then match for 2004 and 12
-db.customers.aggregate([
-    {$unwind:"$payments"},
-    {$addFields:{"year":{$year: "$payments.paymentDate"},"month":{$month: "$payments.paymentDate"}}},
-    {$match:{"year": 2004,"month":12}},
-    {$group: {_id:{$dayOfMonth: "$payments.paymentDate"}, TotalPayments: {$sum: 1}}},
-    {$sort: {_id: 1}}
-])
-
-// Calculate the dollar value of each product in inventory. You can calculate this by multiplying the quantity in stock by the buy price. Display the product name, quantity in stock, buy price, and in its dollar value in a column called “Dollar Value”. Sort the results in descending order based on dollar value.
-db.products.aggregate([
-    {$project:{_id:0,ProductName:"$productName",QuantityinStock:"$quantityInStock",BuyPrice:"$buyPrice",DollarValue:{$multiply:["$quantityInStock","$MSRP"]}}},
-    {$sort:{TotalValue: -1}},
-    {$limit: 25}
-])
----------------
-
 
 // Write a query to create a view named "custByCity" to get a count of how many customers there are in each city.
 db.createView(
@@ -135,14 +76,11 @@ db.createView(
     "orders",
     [
         {$unwind:"$orderDetails"},
-        {$group: {_id: "$customerName", TotalOrder: {$sum: "$orderDetails.priceEach"}}}
+        {$addFields:{"orderTotal":{$multiply:["$orderDetails.quantity","$orderDetails.priceEach"]}}},
+        {$group: {_id: {"customerName": "$customerName", "orderNumber": "$orderNumber"}, TotalOrder: {$sum: "$orderTotal"}}}
     ]
 )
-
     // Query the orderTotals view to select the top 15 orders.
     db.orderTotals.find().sort({TotalOrder: -1}).limit(15)
-
-
-
 
 
