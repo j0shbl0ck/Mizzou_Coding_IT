5d98efa9802a268d8f454ca59d292a92bda46cc2
Josh Block <247jblock@gmail.com>
2022-04-07 21:12:44 -0500

    🔨  added options 4-6

--- INFOTC_3530/lab10[JSBVNN].sh
+++ INFOTC_3530/lab10[JSBVNN].sh
@@ -3,7 +3,7 @@
 #     This script allows user interactiveness across the console
 # .DESCRIPTION
 #     Author: j0shbl0ck https://github.com/j0shbl0ck
-#     Version: 1.0.1
+#     Version: 1.0.2
 #     Date: 04.07.22
 #     Type: Public
 # .NOTES
@@ -37,7 +37,6 @@ while [ $choice -lt 1 ] || [ $choice -gt 6 ]
 do
     echo "Invalid choice"
     read -p "Enter your choice [1-6]: " choice
-done
 
     # ===== Operating system info =====
     # if option 1 is chosen from main menu create os function with 5 options
@@ -211,18 +210,21 @@ done
         # if option 1 is chosen from file menu ask to create a file then return to main menu
         if [ $choice -eq 1 ]; then
             read -p "Enter file name: " filename
-            # check if file exists and if so, do you want to overwrite it else create a new file
+            # check if file exists and if so, ask user if they want to overwrite it, if yes, over file, if no, return to main menu
             if [ -f $filename ]; then
-                read -p "File exists. Do you want to overwrite it? [y/n]: " choice
-                if [ $choice == "y" ]; then
+                read -p "File exists, overwrite? [y/n]: " overwrite
+                if [ $overwrite == "y" ]; then
                     touch $filename
                     echo "*******************************"
                     main_menu
-                else
-                    echo "*******************************"
+                elif [ $overwrite == "n" ]; then
                     main_menu
-                fi
-            else
+                else
+                    # if y or n is not entered ask again
+                    echo "Invalid choice"
+                        read -p "Do you want to back them up to backup_bonus.tar? [yY/n]: " choice
+                    fi
+            else    
                 touch $filename
                 echo "*******************************"
                 main_menu
@@ -293,24 +295,47 @@ done
             echo "1. Find files by name"
             echo "2. Find files by size"
             echo "3. Find files by date"
-            echo "4. Find files by owner"
-            echo "5. Find files by group"
-            echo "6. Return to main menu"
+            echo "4. Find files by type"
+            echo "5. Find files by owner"
+            echo "6. Find files by group"
+            echo "7. Return to main menu"
         }
         # Get user's choice
-        read -p "Enter your choice [1-6]: " choice
-        # repeat question if number is not between 1 and 6
-        while [ $choice -lt 1 ] || [ $choice -gt 6 ]
+        read -p "Enter your choice [1-7]: " choice
+        # repeat question if number is not between 1 and 7
+        while [ $choice -lt 1 ] || [ $choice -gt 7 ]
         do
             echo "Invalid choice"
-            read -p "Enter your choice [1-6]: " choice
+            read -p "Enter your choice [1-7]: " choice
         done
         # if option 1 is chosen from find menu ask to find files by name then return to main menu
         if [ $choice -eq 1 ]; then
             read -p "Enter file name: " filename
             find . -name $filename
-            echo "*******************************"
-            main_menu
+            # if files are found, ask if user wants to back them up to backup_bonus.tar else return to main menu
+            if [ -f $filename ]; then
+                read -p "Do you want to back them up to backup_bonus.tar? [yY/n]: " choice
+                # if user presses y or Y, create a tar file with the files found or if user presses n or N, return to main menu else repeat question
+                if [ $choice == "y" ] || [ $choice == "Y" ]; then
+                    # check if tar file exists, if so, add files to tar file else create tar file and add files to it
+                    if [ -f backup_bonus.tar ]; then
+                        tar -rvf backup_bonus.tar $filename
+                    else
+                        tar -rvf backup_bonus.tar $filename
+                    fi
+                    echo "*******************************"
+                    main_menu
+                elif [ $choice == "n" ] || [ $choice == "N" ]; then
+                    echo "*******************************"
+                    main_menu
+                else
+                    echo "Invalid choice"
+                    read -p "Do you want to back them up to backup_bonus.tar? [yY/n]: " choice
+                fi
+            else
+                echo "*******************************"
+                main_menu
+            fi
             # if option 2 is chosen from find menu ask to find files by size then return to main menu
         elif [ $choice -eq 2 ]; then
             read -p "Enter file size: " filesize
@@ -319,19 +344,56 @@ done
             main_menu
             # if option 3 is chosen from find menu ask to find files by date then return to main menu
         elif [ $choice -eq 3 ]; then
-            read -p "Enter date: " date
-            find . -mtime $date
+            read -p "Enter file date: " filedate
+            find . -mtime $filedate
             echo "*******************************"
             main_menu
-            # if option 4 is chosen from find menu ask to find files by owner then return to main menu
+            # if option 4 is chosen from find menu ask to find files by type then return to main menu
         elif [ $choice -eq 4 ]; then
-            read -p "Enter owner: " owner
-            find . -user $owner
+            read -p "Enter file type: " filetype
+            find . -type $filetype
+            # ask user if they would like to change the file type
+            read -p "Do you want to change the file type? [yY/nN]: " choice
+            # if user presses y or Y, ask user to enter new file type else return to main menu
+            if [ $choice == "y" ] || [ $choice == "Y" ]; then
+                read -p "Enter new file type: " newfiletype
+                find . -type $newfiletype
+                echo "*******************************"
+                main_menu
+            elif [ $choice == "n" ] || [ $choice == "N" ]; then
+                echo "*******************************"
+                main_menu
+            else
+                echo "Invalid choice"
+                read -p "Do you want to change the file type? [yY/nN]: " choice
+            fi
             echo "*******************************"
             main_menu
-            # if option 5 is chosen from find menu ask to find files by group then return to main menu
+            # if option 5 is chosen from find menu ask to find files by owner then return to main menu
         elif [ $choice -eq 5 ]; then
-            read
+            read -p "Enter file owner: " fileowner
+            find . -user $fileowner
+            echo "*******************************"
+            main_menu
+            # if option 6 is chosen from find menu ask to find files by group then return to main menu
+        elif [ $choice -eq 6 ]; then
+            read -p "Enter file group: " filegroup
+            find . -group $filegroup
+            echo "*******************************"
+            main_menu
+            # if option 7 is chosen from find menu return to main menu
+        elif [ $choice -eq 7 ]; then
+            main_menu
+        fi
+    fi
+
+    # ===== Exit =====
+    # if option 6 is chosen from main menu exit the program
+    if [ $choice -eq 6 ]; then
+        exit
+    fi
+done
+
 
 
 
