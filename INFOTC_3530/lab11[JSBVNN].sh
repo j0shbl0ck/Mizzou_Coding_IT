e2195d1c1da69772008011b539d4a1911b100f8d
Josh Block <247jblock@gmail.com>
2022-04-15 21:22:29 -0500

    ⚠️  script under development

--- INFOTC_3530/lab11[JSBVNN].sh
+++ INFOTC_3530/lab11[JSBVNN].sh
@@ -3,7 +3,7 @@
 #     This script allows user interactiveness across the console
 # .DESCRIPTION
 #     Author: j0shbl0ck https://github.com/j0shbl0ck
-#     Version: 1.0.1
+#     Version: 1.0.2
 #     Date: 04.15.22
 #     Type: Public
 # .NOTES
@@ -34,62 +34,46 @@ do
 
         case $choice in
                 1)
-                        echo "=====================Auditing user accounts====================="
-                        echo "-> List of users created after Linux installation:"
-                        echo $(awk -F: '($3>999 && $3<65534) {print $1}' /etc/passwd)
-                        echo "-> List of users with elevated privileges added to the sudoers file:"
-                        echo $(cat /etc/sudoers | grep ALL= | grep ^[^%#])
-                        echo "-> List of users added to the wheel or sudo:"
-                        echo $(cat /etc/group | grep 'sudo\|wheel' | awk -F: '{print $4}')
+                    echo "=====================Auditing user accounts====================="
+                    # Display username, exact date/time and IP address for the users who logged in to the system, ordered by date (oldest to newest).
+                    echo "-> List of users who logged in to the system:"
+                    echo $(last | grep -v "Never" | awk '{print $1, $3, $4}')
+                    # Display in ascending order the date and time when the last user account was created.
+                    echo "-> Last user account created:"
+                    echo $(last | grep -v "Never" | awk '{print $1, $3, $4}' | sort -k3 | tail -1)
+                    # Display the usernames who are currently online
+                    echo "-> List of users who are currently online:"
+                    echo $(who | awk '{print $1}')
                 ;;
                 2)
-                        echo "=====================Auditing Hard Disk Drives (HDD)====================="
-                        echo "-> List of mounted drives:"
-                        echo $(mount | cut -d' ' -f1)
-                        echo "-> List of mounted drives with their mount points:"
-                        echo $(mount | cut -d' ' -f1,2)
-                        echo "-> List of mounted drives with their mount points and filesystems:"
-                        echo $(mount | cut -d' ' -f1,2,3)
-                        echo "-> List of mounted drives with their mount points, filesystems and options:"
-                        echo $(mount | cut -d' ' -f1,2,3,4)
-                        echo "-> List of mounted drives with their mount points, filesystems, options and mount points:"
-                        echo $(mount | cut -d' ' -f1,2,3,4,5)
+                    echo "=====================Auditing Hard Disk Drives (HDD)====================="
+                    # Display the total number of HDD, their names, their partitions, and the mounting point
+                    echo "-> List of Hard Disk Drives (HDD):"
+                    echo $(lsblk | grep disk | awk '{print $1, $4, $7}')
                 ;;
                 3)          
-                        echo "=====================Auditing network configuration====================="
-                        echo "-> List of network interfaces:"
-                        echo $(ifconfig | cut -d' ' -f1)
-                        echo "-> List of network interfaces with their IP addresses:"
-                        echo $(ifconfig | cut -d' ' -f1,2)
-                        echo "-> List of network interfaces with their IP addresses and netmask:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4)
-                        echo "-> List of network interfaces with their IP addresses, netmask and broadcast address:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5)
-                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address and MAC address:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8)
-                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address and status:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9)
-                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status and MTU:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12)
-                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status, MTU and speed:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12,13)
-                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status, MTU, speed and duplex:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12,13,14)
-                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status, MTU, speed and duplex and carrier:"
-                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12,13,14,15)
+                    echo "=====================Auditing network configuration====================="
+                    # Display the network adapter, the IP address, subnet mask, and the MAC address. Format the output to only include the requested info
+                    echo "-> List of network adapters:"
+                    echo $(ifconfig | grep -v "lo" | awk '{print $1, $2, $4, $5}')
+                    # Display services information for the first 100 ports that have the state of LISTEN, omit localhost output (hint: explore the netstat -tulpn command)
+                    echo "-> List of services listening on ports:"
+                    echo $(netstat -tulpn | grep LISTEN | head -n 100)
                 4)
-                        echo "===================Update/upgrade the system==================="  
-                        if [ $system == \"fedora\" ]
-                        then
-                                echo $(sudo yum update -y)
-                        elif [ $system == debian ]
-                        then
-                                echo $(sudo apt update -y)
-                        fi
+                    echo "===================Update/upgrade the system==================="
+                    # Update and upgrade the system (hint: different distributions use different package managers! Use the partial implementation to help with this)
+                    if [ $system == \"fedora\" ]
+                    then
+                            echo $(sudo yum update -y)
+                    elif [ $system == debian ]
+                    then
+                            echo $(sudo apt update -y)
+                    fi
                 ;;
-                5)      echo byei
-                        clear
-                        exit
+                5)      
+                    echo bye
+                    clear
+                    exit
                 ;;
                 *) echo Invalid option
                 ;;
