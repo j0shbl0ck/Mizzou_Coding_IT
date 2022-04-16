5b663e670dce9dad1d5a5b392494533c30ea4889
Josh Block <247jblock@gmail.com>
2022-04-15 20:51:56 -0500

    ⚠️  script under development

--- INFOTC_3530/lab11[JSBVNN].sh
+++ INFOTC_3530/lab11[JSBVNN].sh
@@ -3,7 +3,7 @@
 #     This script allows user interactiveness across the console
 # .DESCRIPTION
 #     Author: j0shbl0ck https://github.com/j0shbl0ck
-#     Version: 1.0.0
+#     Version: 1.0.1
 #     Date: 04.15.22
 #     Type: Public
 # .NOTES
@@ -42,6 +42,41 @@ do
                         echo "-> List of users added to the wheel or sudo:"
                         echo $(cat /etc/group | grep 'sudo\|wheel' | awk -F: '{print $4}')
                 ;;
+                2)
+                        echo "=====================Auditing Hard Disk Drives (HDD)====================="
+                        echo "-> List of mounted drives:"
+                        echo $(mount | cut -d' ' -f1)
+                        echo "-> List of mounted drives with their mount points:"
+                        echo $(mount | cut -d' ' -f1,2)
+                        echo "-> List of mounted drives with their mount points and filesystems:"
+                        echo $(mount | cut -d' ' -f1,2,3)
+                        echo "-> List of mounted drives with their mount points, filesystems and options:"
+                        echo $(mount | cut -d' ' -f1,2,3,4)
+                        echo "-> List of mounted drives with their mount points, filesystems, options and mount points:"
+                        echo $(mount | cut -d' ' -f1,2,3,4,5)
+                ;;
+                3)          
+                        echo "=====================Auditing network configuration====================="
+                        echo "-> List of network interfaces:"
+                        echo $(ifconfig | cut -d' ' -f1)
+                        echo "-> List of network interfaces with their IP addresses:"
+                        echo $(ifconfig | cut -d' ' -f1,2)
+                        echo "-> List of network interfaces with their IP addresses and netmask:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4)
+                        echo "-> List of network interfaces with their IP addresses, netmask and broadcast address:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5)
+                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address and MAC address:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8)
+                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address and status:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9)
+                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status and MTU:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12)
+                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status, MTU and speed:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12,13)
+                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status, MTU, speed and duplex:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12,13,14)
+                        echo "-> List of network interfaces with their IP addresses, netmask, broadcast address, MAC address, status, MTU, speed and duplex and carrier:"
+                        echo $(ifconfig | cut -d' ' -f1,2,4,5,8,9,12,13,14,15)
                 4)
                         echo "===================Update/upgrade the system==================="  
                         if [ $system == \"fedora\" ]
