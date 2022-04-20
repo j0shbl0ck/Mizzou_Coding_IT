c8fa9ea0376c55236ecc4bd817937aad04a1e49d
Josh Block <247jblock@gmail.com>
2022-04-19 20:03:53 -0500

    🔨  inserted tables and information

--- INFOTC_3380/project2_[JSBVNN]/project2_[JSBVNN].sql
+++ INFOTC_3380/project2_[JSBVNN]/project2_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 4/18/22
-Version: 1.0.0 ***/
+Version: 1.0.1 ***/
 
 -- Recall all databases
 SHOW DATABASES;
@@ -20,6 +20,93 @@ INSERT INTO <table in module2> (<columns>) VALUES (<values>);
 -- Select from table
 SELECT <columns> FROM <table in module2> WHERE <column> = <value>;
 
+mysql> SHOW tables;
++--------------------+
+| Tables_in_project2 |
++--------------------+
+| countries          |
+| departments        |
+| dependents         |
+| employees          |
+| jobs               |
+| locations          |
+| regions            |
++--------------------+
+
+mysql> DESCRIBE countries;
++--------------+-------------+------+-----+---------+-------+
+| Field        | Type        | Null | Key | Default | Extra |
++--------------+-------------+------+-----+---------+-------+
+| country_id   | char(2)     | NO   | PRI | NULL    |       |
+| country_name | varchar(40) | YES  |     | NULL    |       |
+| region_id    | int         | NO   | MUL | NULL    |       |
++--------------+-------------+------+-----+---------+-------+
+
+mysql> DESCRIBE departments;
++-----------------+-------------+------+-----+---------+----------------+
+| Field           | Type        | Null | Key | Default | Extra          |
++-----------------+-------------+------+-----+---------+----------------+
+| department_id   | int         | NO   | PRI | NULL    | auto_increment |
+| department_name | varchar(30) | NO   |     | NULL    |                |
+| location_id     | int         | YES  | MUL | NULL    |                |
++-----------------+-------------+------+-----+---------+----------------+
+
+mysql> DESCRIBE dependents;
++--------------+-------------+------+-----+---------+----------------+
+| Field        | Type        | Null | Key | Default | Extra          |
++--------------+-------------+------+-----+---------+----------------+
+| dependent_id | int         | NO   | PRI | NULL    | auto_increment |
+| first_name   | varchar(50) | NO   |     | NULL    |                |
+| last_name    | varchar(50) | NO   |     | NULL    |                |
+| relationship | varchar(25) | NO   |     | NULL    |                |
+| employee_id  | int         | NO   | MUL | NULL    |                |
++--------------+-------------+------+-----+---------+----------------+
+
+mysql> DESCRIBE employees;
++---------------+--------------+------+-----+---------+----------------+
+| Field         | Type         | Null | Key | Default | Extra          |
++---------------+--------------+------+-----+---------+----------------+
+| employee_id   | int          | NO   | PRI | NULL    | auto_increment |
+| first_name    | varchar(20)  | YES  |     | NULL    |                |
+| last_name     | varchar(25)  | NO   |     | NULL    |                |
+| email         | varchar(100) | NO   |     | NULL    |                |
+| phone_number  | varchar(20)  | YES  |     | NULL    |                |
+| hire_date     | date         | NO   |     | NULL    |                |
+| job_id        | int          | NO   | MUL | NULL    |                |
+| salary        | decimal(8,2) | NO   |     | NULL    |                |
+| manager_id    | int          | YES  | MUL | NULL    |                |
+| department_id | int          | YES  | MUL | NULL    |                |
++---------------+--------------+------+-----+---------+----------------+
+
+mysql> DESCRIBE jobs;
++------------+--------------+------+-----+---------+----------------+
+| Field      | Type         | Null | Key | Default | Extra          |
++------------+--------------+------+-----+---------+----------------+
+| job_id     | int          | NO   | PRI | NULL    | auto_increment |
+| job_title  | varchar(35)  | NO   |     | NULL    |                |
+| min_salary | decimal(8,2) | YES  |     | NULL    |                |
+| max_salary | decimal(8,2) | YES  |     | NULL    |                |
++------------+--------------+------+-----+---------+----------------+
+
+mysql> DESCRIBE locations;
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| location_id    | int         | NO   | PRI | NULL    | auto_increment |
+| street_address | varchar(40) | YES  |     | NULL    |                |
+| postal_code    | varchar(12) | YES  |     | NULL    |                |
+| city           | varchar(30) | NO   |     | NULL    |                |
+| state_province | varchar(25) | YES  |     | NULL    |                |
+| country_id     | char(2)     | NO   | MUL | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+mysql> DESCRIBE regions;
++-------------+-------------+------+-----+---------+----------------+
+| Field       | Type        | Null | Key | Default | Extra          |
++-------------+-------------+------+-----+---------+----------------+
+| region_id   | int         | NO   | PRI | NULL    | auto_increment |
+| region_name | varchar(25) | YES  |     | NULL    |                |
++-------------+-------------+------+-----+---------+----------------+
 
 
 -- Write a query to create a view named “EmployeesPerRegion” that shows the region_name and the number of employees from that region in a column called “Number of Employees”.
