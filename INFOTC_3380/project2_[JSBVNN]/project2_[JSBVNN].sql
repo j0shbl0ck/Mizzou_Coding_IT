d1ef1477e05aaac88d72f803343a3afdc5a8a25e
Josh Block <247jblock@gmail.com>
2022-04-20 20:48:43 -0500

    🔨  resolving solutions

--- INFOTC_3380/project2_[JSBVNN]/project2_[JSBVNN].sql
+++ INFOTC_3380/project2_[JSBVNN]/project2_[JSBVNN].sql
@@ -1,7 +1,7 @@
 /*** USER INFORMATION 
 Student: Josh Block
 Date: 4/18/22
-Version: 1.0.4 ***/
+Version: 1.0.5 ***/
 
 -- Recall all databases
 SHOW DATABASES;
@@ -21,7 +21,7 @@ INSERT INTO <table in module2> (<columns>) VALUES (<values>);
 SELECT <columns> FROM <table in module2> WHERE <column> = <value>;
 
 
--- Write a query to create a view named “EmployeesPerRegion” that shows the region_name and the number of employees from that region in a column called “Number of Employees”. [Not created]
+-- Write a query to create a view named “EmployeesPerRegion” that shows the region_name and the number of employees from that region in a column called “Number of Employees”. [Complete]
 CREATE VIEW EmployeesPerRegion AS
 SELECT regions.region_name, COUNT(employees.employee_id) AS Number_of_Employees
 FROM regions
@@ -31,10 +31,10 @@ JOIN departments ON locations.location_id = departments.location_id
 JOIN employees ON departments.department_id = employees.department_id
 GROUP BY regions.region_name;
 
-    -- Query the EmployeesPerRegion to show the number of employees from the Americas. [Not queried]
+    -- Query the EmployeesPerRegion to show the number of employees from the Americas. [Complete]
     SELECT * FROM EmployeesPerRegion WHERE region_name = 'Americas';
 
--- Write a query to create a view named “managers” to display all the managers. Include the manager’s name (first, last), phone number, email, job title, and department name. [Not created]
+-- Write a query to create a view named “managers” to display all the managers. Include the manager’s name (first, last), phone number, email, job title, and department name. [Complete]
 CREATE VIEW managers AS
 SELECT employees.first_name, employees.last_name, employees.phone_number, employees.email, jobs.job_title, departments.department_name
 FROM employees
@@ -43,10 +43,14 @@ JOIN departments ON employees.department_id = departments.department_id
 WHERE jobs.job_title LIKE '%Manager%'
 GROUP BY employees.employee_id;
 
-    -- Query the managers view to show the number of managers in each department. [Not queried]
-    SELECT departments.department_name, COUNT(managers.first_name) AS Number_of_Managers
+    -- Query the managers view to show the number of managers in each department. [Complete]
+    SELECT departments.department_name, COUNT(managers.first_name) AS Number_of_Managers 
+    FROM managers
+    JOIN departments ON managers.department_name = departments.department_name
+    GROUP BY departments.department_name;
 
--- Write a query to create a view named “DependentsByDepartment” to get a count of how many dependents there are in each department. [Not created]
+
+-- Write a query to create a view named “DependentsByDepartment” to get a count of how many dependents there are in each department. [Complete]
 CREATE VIEW DependentsByDepartment AS
 SELECT departments.department_name, COUNT(dependents.dependent_id) AS Number_of_Dependents
 FROM departments
@@ -55,15 +59,15 @@ JOIN employees ON departments.department_id = employees.department_id
 JOIN dependents ON employees.employee_id = dependents.employee_id
 GROUP BY departments.department_name;
 
-    -- Query the DependentsByDepartment view to show the department with the largest number of dependents. This should show the department name and the number of dependents. [Not queried]
-    SELECT * FROM DependentsByDepartment ORDER BY Number_of_Dependents DESC LIMIT 1;
+    -- Query the DependentsByDepartment view to show the department with the largest number of dependents. This should show the department name and the number of dependents. [Complete]
+    SELECT * FROM DependentsByDepartment ORDER BY Number_of_Dependents DESC LIMIT 2;
 
--- Write a query to create a view named “HiresByYear” that calculates the number of employees hired each year. Remember the SQL $year function.
+-- Write a query to create a view named “HiresByYear” that calculates the number of employees hired each year. Remember the SQL $year function. [Complete]
 CREATE VIEW HiresByYear AS
 SELECT YEAR(employees.hire_date) AS Hire_Year, COUNT(employees.employee_id) AS Number_of_Hires
 FROM employees
 GROUP BY Hire_Year;
-    -- Query the HiresByYear view to show the number of hires in 1997. [Not queried]
+    -- Query the HiresByYear view to show the number of hires in 1997. [Complete]
     SELECT * FROM HiresByYear WHERE Hire_Year = 1997;
 
 -- Write a query to create a view named “SalaryByDepartment” to calculate total salaries for each department. [Not created]
