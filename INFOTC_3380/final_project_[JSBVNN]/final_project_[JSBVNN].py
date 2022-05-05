be4d2bbe321d623729a403232ddb0c23bc98a9bc
Josh Block <247jblock@gmail.com>
2022-05-05 14:58:30 -0500

    🔨  fixing update queries

--- INFOTC_3380/final_project_[JSBVNN]/final_project_[JSBVNN].py
+++ INFOTC_3380/final_project_[JSBVNN]/final_project_[JSBVNN].py
@@ -1,7 +1,7 @@
 """ USER INFORMATION 
 Student: Josh Block
 Date: 4/28/22
-Version: 1.4.8 """
+Version: 1.4.9 """
 
 # create SQL connection
 import mysql.connector
@@ -255,9 +255,8 @@ def delete_dependent_data(mycursor, dependent_id):
 
     # Update an employee's first name
 def update_employee_first_name_data(mycursor, first_name, employee_id):
-    sqlquery13 = ('''UPDATE employees SET first_name = %s WHERE employee_id = %s;'''%(first_name, employee_id))
-    #val = (first_name, employee_id)
-    mycursor.execute(sqlquery13)
+    sqlquery13 = '''UPDATE employees SET first_name = %s WHERE employee_id = %s;'''
+    mycursor.execute(sqlquery13, (first_name, employee_id))
     return
 
     # Update an employee's last name
