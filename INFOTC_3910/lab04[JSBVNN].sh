ca97a0d08cad139f86ad8082f19b550e81c9fd57
Josh Block <247jblock@gmail.com>
2022-07-05 19:01:43 -0500

    ⬆️  uploaded new folder and bash script

---
+++ INFOTC_3910/lab04[JSBVNN].sh
@@ -0,0 +1,24 @@
+#  <<
+# .SYNOPSIS
+#     This script allows the user to define the initial and last octet for the 192.168.200.0/24 network in order to ping sweep that particular IP range.
+# .DESCRIPTION
+#     Author: Josh Block
+#     Version: 1.0.7
+#     Date: 04.15.22
+#     Type: Public
+# .NOTES
+#      
+# .LINK
+#     
+# >>
+#! /bin/bash
+
+
+# Ask user for inital octet of 192.168.200.0/24
+echo "Enter the initial octet of the IP range you want to scan: "
+read initial_octet
+
+# Ask user for last octet of 192.168.200.0/24
+echo "Enter the last octet of the IP range you want to scan: "
+read last_octet
+
