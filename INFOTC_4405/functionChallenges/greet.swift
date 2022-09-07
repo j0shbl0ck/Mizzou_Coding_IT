083536ed8e30428f639953f3999c5015385e1fce
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-06 21:50:37 -0500

    ⬆️  uploaded code

---
+++ INFOTC_4405/functionChallenges/greet.swift
@@ -0,0 +1,12 @@
+// Josh Block
+// INFOTC 4405
+// 09.06.2022
+
+import Cocoa
+
+func greet(person: String) -> String {
+    let greeting = "Hello \(person)"
+    return greeting
+}
+
+print(greet(person: "Josh"))
