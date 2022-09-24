1715bc2d822cf4eecee897c39d3ac758e1dbde5c
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-24 15:34:01 -0500

    🔨  added necessary padding

--- INFOTC_4405/SportsSwiftUIDesigns/SportsEventInfo/SportsEventInfo/ContentView.swift
+++ INFOTC_4405/SportsSwiftUIDesigns/SportsEventInfo/SportsEventInfo/ContentView.swift
@@ -12,12 +12,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 10){
-            Spacer()
-            HStack(){
+        VStack(alignment: .leading){
+            HStack {
                 Image("football")
-                    .frame(alignment: .trailing)
-                    .padding()
+                    .padding(10)
                 Text("football")
                 .font(Font.system(size: 24))
                 
@@ -26,12 +24,13 @@ struct ContentView: View {
                 HStack {
                     Text("Matchup:").italic().font(.subheadline)
                     Text("Tennessee vs Missouri").font(.subheadline)
+                    Text("")
                 }
                 HStack {
                     Text("When:").italic().font(.subheadline)
                     Text("October 2, 2021 at 11:30:00 AM").font(.subheadline)
                 }
-            }
+            }.padding(10)
             /*List{
                 VStack(alignment: .leading) {
                     HStack {
@@ -47,15 +46,13 @@ struct ContentView: View {
             VStack(alignment: .center){
                 Image("TNvsMO_Oct2_2022")
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
+                    .aspectRatio(contentMode: .fit)
                     .frame(alignment: .center)
                 Image("TNvsMO_GameLeaders")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 250.0, height: 250.0, alignment: .center)
             }
-            
-
         }
     }
 }
