f222e69eb70a8f238d39ad7b894311e2f76d01f0
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-10-23 19:47:58 -0500

    ⬆️   uploaded code for source control

---
+++ INFOTC_4405/midtermProject/midtermProject/midtermProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  midtermProjectApp.swift
+//  midtermProject
+//
+//  Created by Josh Block on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct midtermProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
