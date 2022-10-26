887b60beaefd18d6f365a5f0923fc9a7ad73aad4
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-10-25 23:21:22 -0500

    ⬆️  uploaded capstone testing pages

---
+++ INFOTC_4970W/Capstone_Gophish/apiTesting/api_samples.js
@@ -0,0 +1,18 @@
+[{
+    "name": "Example Group",
+    "targets": [
+    {
+        "email": "user@example.com",
+        "first_name": "Example",
+        "last_name": "User",
+        "position": ""
+    },
+    {
+        "email": "foo@bar.com",
+        "first_name": "Foo",
+        "last_name": "Bar",
+        "position": ""
+    }
+    ]
+}
+]
