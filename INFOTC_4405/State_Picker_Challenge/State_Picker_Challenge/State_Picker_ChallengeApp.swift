459188664bd4d5ba5adfaf7c2e85dd0ec304bf94
Josh Block <0go1fbn9c@relay.firefox.com>
2022-10-31 16:59:03 -0500

    ⬆️  uploaded code

---
+++ INFOTC_4405/State_Picker_Challenge/State_Picker_Challenge/State_Picker_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_Picker_ChallengeApp.swift
+//  State_Picker_Challenge
+//
+//  Created by Josh Block on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct State_Picker_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
