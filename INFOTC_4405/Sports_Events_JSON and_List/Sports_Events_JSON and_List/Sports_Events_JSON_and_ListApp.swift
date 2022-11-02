ed9b744d4a17f9990d7adf03eb6dc4ab06aa9c57
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-02 14:32:57 -0500

    ☔️  formating code

--- INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/Sports_Events_JSON_and_ListApp.swift
+++ INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/Sports_Events_JSON_and_ListApp.swift
@@ -7,13 +7,26 @@
 
 import SwiftUI
 
-struct EventData: Decodable, Identifiable {
+struct EventData: Codable, Identifiable {
+    var id = UUID()
+    let status: String
+    var events: [Event]
+
+    enum CodingKeys: String, CodingKeys {
+        case status
+        case events
+    }
+}
+
+struct Event: Codable, Identifiable {
     var id = UUID()
     let sport: String
     let matchup: String
     let date: Date
-    enum Category: String, Decodable {
-        case swift, combine, debugging, xcode
+    enum CodingKeys: String, Decodable {
+        case sport
+        case matchup
+        case date
     }
 }
 
@@ -21,13 +34,10 @@ class EventLoader {
     class func load(jsonFileName: String) -> EventData? {
         var EventData: EventData?
         let jsonDecoder = JSONDecoder()
-        jsonDecoder.dateDecodingStrategy = .iso8601
-
         if let jsonFileUrl = Bundle.main.url(forResource: jsonFileName, withExtension: ".json") {
-                let jsonData = try? Data(contentsOf: jsonFileUrl) {
-                    EventData = try?  jsonDecoder.decode(EventData.self, from: jsonData)
-                }
-        } 
+                let jsonData = try? Data(contentsOf: jsonFileUrl)
+                EventData = try? jsonDecoder.decode(EventData.self, from: jsonData)
+        }
         return EventData
     }
 }
@@ -35,7 +45,6 @@ class EventLoader {
 
 @main
 struct Sports_Events_JSON_and_ListApp: App {
-    
     let EventData: EventData?
 
     init() {
@@ -43,9 +52,10 @@ struct Sports_Events_JSON_and_ListApp: App {
         if let EventData = EventData {
             print("Status: \(EventData.status)")
             for event in EventData.events {
-                print("Sport: \(event.sport)")
-                print("Matchup: \(event.matchup)")
-                print("Date: \(event.date)")
+                print("id = \(event.id)")
+                print("sport = \(event.sport)")
+                print("matchup = \(event.matchup)")
+                print("date = \(event.date)")
             }
         }
     }
