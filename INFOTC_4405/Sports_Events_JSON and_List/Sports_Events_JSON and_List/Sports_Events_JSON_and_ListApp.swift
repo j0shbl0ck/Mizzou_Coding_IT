a1620d4d172953db570a9c82441d5b22d830585d
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-02 13:22:12 -0500

    🦦  added JSON reading check

--- INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/Sports_Events_JSON_and_ListApp.swift
+++ INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/Sports_Events_JSON_and_ListApp.swift
@@ -9,6 +9,21 @@ import SwiftUI
 
 @main
 struct Sports_Events_JSON_and_ListApp: App {
+    
+    init() {
+        if let filepath = Bundle.main.path(forResource: "measurements", ofType: "json") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                // the information in the file is in contents
+                print(contents)  // to show that it is there
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // measurements.json is cannot be found
+        }
+    }
+        
     var body: some Scene {
         WindowGroup {
             ContentView()
