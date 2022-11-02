1fa5ea63663f845b66514065a9a2327d35962fb8
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-02 13:17:34 -0500

    ⬆️  uploaded swift folder

---
+++ INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sports_Events_JSON and_List
+//
+//  Created by Josh Block on 11/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/Sports_Events_JSON_and_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_Events_JSON_and_ListApp.swift
+//  Sports_Events_JSON and_List
+//
+//  Created by Josh Block on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Sports_Events_JSON_and_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
