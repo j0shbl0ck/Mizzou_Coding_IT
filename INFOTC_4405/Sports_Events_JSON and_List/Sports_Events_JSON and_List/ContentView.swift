bfa5ecd6566a96433bcd1aa141914ad4d5f0b4dd
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-04 20:00:32 -0500

    🪢 building list function

--- INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/ContentView.swift
+++ INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+     List(users) { user in
+        Text(user.username)
+            .font(.headline)
+        Text(user.name)
+            .font(.subheadline)  
         }
-        .padding()
     }
 }
 
