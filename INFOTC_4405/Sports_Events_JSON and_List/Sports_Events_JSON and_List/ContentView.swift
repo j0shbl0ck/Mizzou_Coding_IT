8247d21c8f155f1c15012ad1d0e2a8a09b2626f5
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-04 22:23:45 -0500

    🦦   ugh this is soooo good

--- INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/ContentView.swift
+++ INFOTC_4405/Sports_Events_JSON and_List/Sports_Events_JSON and_List/ContentView.swift
@@ -89,15 +89,22 @@ struct ContentView: View {
     var body: some View {
         var eventData: EventData? = EventLoader.load(jsonFileName: "sports_events")
         
-        // build the list
+        // build the list with image and text
         List(eventData!.events) { event in
-            VStack(alignment: .leading) {
-                Text(event.sport)
-                    .font(.headline)
-                Text(event.matchup)
-                    .font(.subheadline)
-                Text(event.date, style: .date)
-                    .font(.subheadline)
+            HStack {
+                // for each sport, get sport then get the image
+                Image(event.sport)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            
+                VStack(alignment: .leading) {
+                    Text(event.sport)
+                        .font(.headline)
+                    Text(event.matchup)
+                        .font(.subheadline)
+                    Text(event.date.description)
+                        .font(.subheadline)
+                }
             }
         }
     }
