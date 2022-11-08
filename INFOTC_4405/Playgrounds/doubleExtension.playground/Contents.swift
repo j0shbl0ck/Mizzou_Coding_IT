f22086ec7c05ada38582ccbdafd5b175da7a1d7e
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-07 22:24:44 -0600

    🏗️  building code

--- INFOTC_4405/Playgrounds/doubleExtension.playground/Contents.swift
+++ INFOTC_4405/Playgrounds/doubleExtension.playground/Contents.swift
@@ -1,3 +1,22 @@
+// Josh Block
+// INFOTC 4405
+// 11.07.2022
+
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+
+// create an extension that adds a new method called "add" and addes Double String type
+extension String {
+    func add(_ value: Double) -> Double? {
+        guard let double = Double(self) else {
+            return nil
+        }
+        return double + value
+    }
+}
+
+// test the extension
+
