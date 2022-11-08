57e90875fa87ca8e629c3859b90a990fc2ff68a0
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-07 22:33:18 -0600

    🦁 uploaded code

--- INFOTC_4405/Playgrounds/doubleExtension.playground/Contents.swift
+++ INFOTC_4405/Playgrounds/doubleExtension.playground/Contents.swift
@@ -4,21 +4,16 @@
 
 import UIKit
 
-var greeting = "Hello, playground"
-
-
-
-// create an extension that adds a new method called "add" and addes Double String type
-extension String {
-    func add(_ value: Double) -> Double? {
-        guard let double = Double(self) else {
+// create an extension that adds a new method called "add" and adds Double String type
+extension Double {
+    func add(_ value: String) -> Double? {
+        guard let numValue = Double(value) else {
             return nil
         }
-        return double + value
+        return self + numValue
     }
 }
 
 // test the extension
 let value1: Double? = 3.5.add("1.2") // value1 will equal 4.7
 let value3: Double? = 1.5.add("words") // value will be nil
-
