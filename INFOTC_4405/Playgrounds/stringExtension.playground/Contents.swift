f5a02704340d62e17e165b3a8875ab44621ba290
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-07 21:54:23 -0600

    🪖 completed assignment

--- INFOTC_4405/Playgrounds/stringExtension.playground/Contents.swift
+++ INFOTC_4405/Playgrounds/stringExtension.playground/Contents.swift
@@ -1,3 +1,25 @@
+// Josh Block
+// INFOTC 4405
+// 11.07.2022
+
 import UIKit
 
-var greeting = "Hello, playground"
+// create an extension to the String type that adds a new method called "add" that receives an integer and returns an optional string
+extension String {
+    func add(_ value: Int) -> Int? {
+        guard let integer = Int(self) else {
+            return nil
+        }
+        return integer + value
+    }
+}
+
+// test the new method
+let value1: Int? = "5".add(3) // value1 will equal 8
+print(value1 as Any)
+
+let value2: Int? = "-2".add(1) // value2 will equal -1
+print(value2 as Any)
+
+let value3: Int? = "words".add(4) // value3 will be nil
+print(value3 as Any)
