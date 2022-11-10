c7fb7abcf8ec50d89c337f16b6513afbac4083bd
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-10 13:30:57 -0600

    🦦  PUSHING ContentView.swift

--- INFOTC_4405/Field_Survey_Challenge/Field_Survey_Challenge/ContentView.swift
+++ INFOTC_4405/Field_Survey_Challenge/Field_Survey_Challenge/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
+// displays date in a human readable format
+private var dateformatter: DateFormatter {
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "MMMM dd, yyyy"
+    return dateformatter
+}
+
+// displays the time in a human readable format
+private var timeFormatter: DateFormatter {
+    let timeformatter = DateFormatter()
+    timeformatter.dateFormat = "h:mm:ss a"
+    return timeformatter
+}
+
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        var fieldData: FieldData? = FieldLoader.load(jsonFileName: "field_observations")
+
+        // build navigation view
+        NavigationView {
+            // build the list with image and text
+            List(fieldData!.observations) { observation in
+                HStack {
+                    // for each animal, get the classification, title, and date
+                    Image(observation.classification)
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .padding()
+                    VStack(alignment: .leading) {
+                        Text(observation.title)
+                            .font(.headline)
+                        // display the date at time in a human readable format
+                        Text("\(observation.date, formatter: dateformatter) at \(observation.date, formatter: timeFormatter)")
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .navigationBarTitle("Field Survey")
         }
-        .padding()
     }
 }
 
