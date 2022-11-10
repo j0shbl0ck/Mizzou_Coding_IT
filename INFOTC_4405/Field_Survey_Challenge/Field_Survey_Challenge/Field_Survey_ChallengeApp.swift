f3affc111781174d9cd3290fdb6adc306dd3b288
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-10 13:03:45 -0600

    ⬆️  uploaded necessary code

---
+++ INFOTC_4405/Field_Survey_Challenge/Field_Survey_Challenge/Field_Survey_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Field_Survey_ChallengeApp.swift
+//  Field_Survey_Challenge
+//
+//  Created by Josh Block on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Field_Survey_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
