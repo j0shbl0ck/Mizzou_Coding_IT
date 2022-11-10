00c730c54852742c16f609849da67d8f548622fa
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-10 13:10:05 -0600

    🦺  inserted structure code

--- INFOTC_4405/Field_Survey_Challenge/Field_Survey_Challenge/Field_Survey_ChallengeApp.swift
+++ INFOTC_4405/Field_Survey_Challenge/Field_Survey_Challenge/Field_Survey_ChallengeApp.swift
@@ -7,6 +7,46 @@
 
 import SwiftUI
 
+struct FieldData: Codable, Identifiable {
+    var id = UUID()
+    let status: String
+    var observations: [Field]
+    
+    enum CodingKeys: String,CodingKey {
+        case status
+        case observations
+    }
+}
+
+struct Field: Codable, Identifiable {
+    var id = UUID()
+    let classification: String
+    let title: String
+    let description: String
+    let date: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case classification
+        case title
+        case description
+        case date
+    }
+}
+
+class FieldLoader {
+    class func load(jsonFileName: String) -> FieldData? {
+        var fieldData: FieldData?
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.dateDecodingStrategy = .iso8601
+        
+        if let jsonFileUrl = Bundle.main.url(forResource: jsonFileName, withExtension: ".json"),
+            let jsonData = try? Data(contentsOf: jsonFileUrl) {
+                fieldData = try? jsonDecoder.decode(FieldData.self, from: jsonData)
+        }
+        return fieldData
+    }
+}
+
 @main
 struct Field_Survey_ChallengeApp: App {
     var body: some Scene {
